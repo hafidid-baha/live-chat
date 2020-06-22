574cb7a616e6194ffb67e9088560c74c27031cb8
Evel25 <bi3.h300@gmail.com>
2020-06-22 15:27:24 +0100

    update the server file to work with browser

--- bin/server.php
+++ bin/server.php
@@ -2,11 +2,17 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Socket\Chat;
+use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 
 
     $server = IoServer::factory(
-        new Chat(),
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
         8080
     );
 
